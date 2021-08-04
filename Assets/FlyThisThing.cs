e97983be70e7057b5435cb87f78084699fa56a31
Kornvik Tanpipat <40062331+kornsinanju@users.noreply.github.com>
2021-08-04 15:48:54 +0900

    UFO fly!!

--- Assets/FlyThisThing.cs
+++ Assets/FlyThisThing.cs
@@ -6,26 +6,28 @@ using Photon.Pun;
 public class FlyThisThing : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-    private float tx, ty, tz, fx, fy, fz,dtx,dty,dtz;
+    public PhotonView photonView;
+    private float tx, ty, tz, fx, fy, fz;
     private Rigidbody rb;
     // public PhotonView photonView;
     // private PhotonView photonView;
     // [SerializeField]
     // [Range(0,20)]
-    public float Kp = 1,Ki=0,Kd=0;
-    private float error=0,prev_error=0,targety=2,ex=0,ez=0,targetz=2,targetx=2;
+    public float Kp = 2,Ki=0,Kd=0;
+    private float error=0,prev_error=0,targety=2,etx=0,etz=0,targetz=2,targetx=2,prev_etx=0,prev_etz=0;
     private float curx,cury,curz;
     private float p=0,i=0,d=0;
+    private float ptx=0,itx=0,dtx=0;
+    private float ptz=0,itz=0,dtz=0;
     private float initialy=0;
 
-    public float stability = 0.3f;
-    public float speed = 2.0f;
+    public float stability = 1.0f;
+    public float speed = 3.0f;
     void Start()
     {
 
         rb=this.GetComponent<Rigidbody>();
-        // photonView = this.GetComponent<PhotonView>();
+        photonView = this.GetComponent<PhotonView>();
         targety = rb.position[1];
         targetx = rb.position[0];
         targetz = rb.position[2];
@@ -47,7 +49,7 @@ public class FlyThisThing : MonoBehaviour
         ty=0.0f;
         tz=0.0f;
 
-        // if (photonView.IsMine)
+        if (photonView.IsMine)
         {
             if (Input.GetKey(KeyCode.A))
             {
@@ -66,12 +68,12 @@ public class FlyThisThing : MonoBehaviour
             if (Input.GetKey(KeyCode.P))
             {
                 targety += 0.01F;
-                fy= 20.0f;
+                fy= 15.0f;
             }
             else if (Input.GetKey(KeyCode.L)&& targety>initialy)
             {
                 targety -= 0.01F;
-                fy = -20.0f;
+                fy = -15.0f;
             }
             else 
                 fy = -rb.velocity[1]*2.0F;
@@ -80,48 +82,52 @@ public class FlyThisThing : MonoBehaviour
             else if (Input.GetKey(KeyCode.W))
                 fz = -20.0f;
             else
-                fz = -rb.velocity[2]*0.6F;
+                fz = -rb.velocity[2]*2.0F;
 
             if (targety<initialy)
                 targety = initialy;
-            // fy = p*(targety-rb.position[1]);
-            
-            // if (Input.GetKey(KeyCode.D))
-            //     tx= 10.0f;
-            // if (Input.GetKey(KeyCode.A))
-            //     tx = -10.0f;
+
             if (Input.GetKey(KeyCode.Q))
-                ty= 3.0f;
+                ty= 15.0f;
             else if (Input.GetKey(KeyCode.E))
-                ty = -3.0f;
+                ty = -15.0f;
             else 
                 ty = -rb.angularVelocity[1]*1.0F;
         }
-        // if (Input.GetKey(KeyCode.E))
-        //     tz= 10.0f;
-        // if (Input.GetKey(KeyCode.Z))
-        //     tz = -10.0f;
-        // Debug.Log(fx);
-        Quaternion rot = rb.rotation;
-        Vector3 ang = rb.angularVelocity;
-        ex = rot[0];
-        ez = rot[2];
-        // tx = -ex*Kp;
-        // tz = -ez*Kp;
-        Vector3 predictedUp = Quaternion.AngleAxis(rb.angularVelocity.magnitude*Mathf.Rad2Deg*stability/speed,rb.angularVelocity)*transform.up;
-        Vector3 torqueVector = Vector3.Cross(predictedUp,Vector3.up);
-        rb.AddTorque(torqueVector * speed * speed);
-        // Vector3
-        // ty = 0.0f;
-        Debug.Log(ex.ToString() + " " + ez.ToString());
-        Vector3 nf = new Vector3(0,rb.mass * Mathf.Abs(Physics.gravity.y),0);
+        
+        //---------------------Hovering & Position Control ----------------------
+        
+        Vector3 nf = new Vector3(0,3.5f * Mathf.Abs(Physics.gravity.y),0);
         rb.AddForce(nf);
         Vector3 f= new Vector3 (fx, fy, fz);
 
+        //----------------------- Rotation control ---------------------
+        Quaternion rot = rb.rotation;
+        Vector3 ang = rb.angularVelocity;
+        etx = 0.0f-rot[0];
+        etz = 0.0f-rot[2];
+        ptx = etx;
+        ptz = etz;
+        itx += etx*0.02f;
+        itz += etz*0.02f;
+        dtx = (etx-prev_etx)/0.02f;
+        dtz = (etz-prev_etz)/0.02f;
+        Debug.Log(etx.ToString() + " " + etz.ToString());
+        prev_etx = etx;
+        prev_etz = etz;
+
+        tx = ptx*Kp + itx*Ki + dtx*Kd;
+        tz = ptz*Kp + itz*Ki + dtz*Kd;
         //rb.AddRelativeForce(f);
         rb.AddForce(f);
         Vector3 t= new Vector3 (tx, ty, tz);
-        //rb.AddRelativeTorque(t);
-        rb.AddTorque(t);
+        Debug.Log(ptx.ToString() + " " + ptz.ToString());
+        Debug.Log("Torque " + tx.ToString() + " " + tz.ToString());
+        
+        Vector3 predictedUp = Quaternion.AngleAxis(rb.angularVelocity.magnitude*Mathf.Rad2Deg*stability/speed,rb.angularVelocity)*transform.up;
+        Vector3 torqueVector = Vector3.Cross(predictedUp,Vector3.up);
+        rb.AddTorque(torqueVector * speed * speed);
+        // rb.AddRelativeTorque(t);
+        // rb.AddTorque(t);
     }
 }
--- Assets/rotateCover.cs
+++ Assets/rotateCover.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class rotateCover : MonoBehaviour
 {
     private HingeJoint hinge = null;
+    public PhotonView photonView;
 
     public float force = 0.06f;
     // Start is called before the first frame update
       void Start()
     {
         hinge = GetComponent<HingeJoint>();
-
+        photonView = this.GetComponent<PhotonView>();
         // Make the hinge motor rotate with 90 degrees per second and a strong force.
         var motor = hinge.motor;
         motor.force = 0.06f;
@@ -22,26 +24,30 @@ public class rotateCover : MonoBehaviour
     }
     void Update()
     {
-        var motor = hinge.motor;
-        if (Input.GetKey(KeyCode.F))
+        
+        if (photonView.IsMine)
         {
-            Debug.Log("Open");
-            motor.force = force;
-            motor.targetVelocity = 40;
+            var motor = hinge.motor;
+            if (Input.GetKey(KeyCode.F))
+            {
+                Debug.Log("Open");
+                motor.force = force;
+                motor.targetVelocity = 40;
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                Debug.Log("Close");
+                motor.force = force;
+                motor.targetVelocity = -40;
+            }
+            else
+            {
+                // Debug.Log("Non"); 
+                motor.force = force;
+                motor.targetVelocity = 0;
+            }
+            motor.freeSpin = false;
+            hinge.motor = motor;
         }
-        else if (Input.GetKey(KeyCode.R))
-        {
-            Debug.Log("Close");
-            motor.force = force;
-            motor.targetVelocity = -40;
-        }
-        else
-        {
-            // Debug.Log("Non"); 
-            motor.force = force;
-            motor.targetVelocity = 0;
-        }
-        motor.freeSpin = false;
-        hinge.motor = motor;
     }
 }
